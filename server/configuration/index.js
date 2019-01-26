04de9536559842adcd7fde81b959d587d6c9cf66
haru-lyle <rexor.lyle@gmail.com>
2019-01-26 15:40:00 +0900

    Update

--- server/configuration/index.js
+++ server/configuration/index.js
@@ -17,12 +17,12 @@ if (process.env.NODE_ENV === 'test') {
     JWT_SECRET: 'balikaralauthentication',
     oauth: {
       google: {
-        clientID: '293000110428-lm6klam4patr7ojnk0e9md79gkip32jd.apps.googleusercontent.com',
-        clientSecret: '9rEBRvn1EsK_Nxq2lNcjG085',
+        clientID: '1',
+        clientSecret: '1',
       },
       facebook: {
-        clientID: '344679316117018',
-        clientSecret: '101d522a604515ea7ff3eaa8110391a7',
+        clientID: '1',
+        clientSecret: '1',
       },
     },
   };
