7311d7d78bc441f0e5ba4055c2a3e19282bb0ccc
root <root@vps.balikaral.com>
2019-02-09 19:25:36 -0500

    pdate api

--- server/configuration/index.js
+++ server/configuration/index.js
@@ -3,12 +3,12 @@ if (process.env.NODE_ENV === 'test') {
     JWT_SECRET: 'balikaralauthentication',
     oauth: {
       google: {
-        clientID: 'number',
-        clientSecret: 'string',
+        clientID: '197880274985-qae1s30vumso0tbr25iaf9lrk89e3tp7.apps.googleusercontent.com',
+        clientSecret: '_HgQ8_DZkD-HQlfU9TFVGPEX'
       },
       facebook: {
-        clientID: 'number',
-        clientSecret: 'string',
+        clientID: '521442691675915',
+        clientSecret: 'c89012f33d778db73da4f7d9c9d6e793',
       },
     },
   };
@@ -17,12 +17,12 @@ if (process.env.NODE_ENV === 'test') {
     JWT_SECRET: 'balikaralauthentication',
     oauth: {
       google: {
-        clientID: '1',
-        clientSecret: '1',
+        clientID: '197880274985-qae1s30vumso0tbr25iaf9lrk89e3tp7.apps.googleusercontent.com',
+        clientSecret: '_HgQ8_DZkD-HQlfU9TFVGPEX'
       },
       facebook: {
-        clientID: '1',
-        clientSecret: '1',
+        clientID: '521442691675915',
+        clientSecret: 'c89012f33d778db73da4f7d9c9d6e793'
       },
     },
   };
