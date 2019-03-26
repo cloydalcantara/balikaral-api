bde5876a9254fdde0aec43a2d9d979305af12495
cloydacantara <ten88TEN88@Cloyd.local>
2019-03-26 14:44:06 +0800

    update reviewer management

--- server/models/reviewer-management.js
+++ server/models/reviewer-management.js
@@ -43,7 +43,8 @@ const reviewerManagementSchema = new Schema({
       user: {
         type: Schema.Types.ObjectId,
         ref: "user"
-      }
+      },
+      reason: String
     }
   ],
   validation: Boolean,
