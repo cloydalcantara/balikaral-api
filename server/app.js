5fe0a7ec3cca380051688f2b7c2c6cca8dd69de7
cloydalcantara <alcantaracloyd26@gmail.com>
2020-05-26 14:20:43 +0800

    update

--- server/app.js
+++ server/app.js
@@ -23,27 +23,27 @@ if (!process.env.NODE_ENV === 'test') {
 app.use('/api', express.static('uploads'));
 app.use(bodyParser.json());
 // Routes
-app.use('/balikaral', require('./routes/users'));
-app.use('/balikaral', require('./routes/learningStrand'));
-app.use('/balikaral', require('./routes/level'));
-app.use('/balikaral', require('./routes/exam-management'));
-app.use('/balikaral', require('./routes/exam-type-management'));
-app.use('/balikaral', require('./routes/reviewer-management'));
-app.use('/balikaral', require('./routes/generated-exam'));
-app.use('/balikaral', require('./routes/learningStrandSub'));
-app.use('/balikaral', require('./routes/forum'));
-app.use('/balikaral', require('./routes/comment'));
-app.use('/balikaral', require('./routes/management-forum'));
-app.use('/balikaral', require('./routes/examination-result'));
-app.use('/balikaral', require('./routes/session-guide-management'));
-app.use('/balikaral', require('./routes/survey-management'));
-app.use('/balikaral', require('./routes/survey-user'));
-app.use('/balikaral', require('./routes/landing-page'));
-app.use('/balikaral', require('./routes/auditTrail'));
-app.use('/balikaral', require('./routes/site-instruction'));
-app.use('/balikaral', require('./routes/teacher-learner'));
-app.use('/balikaral', require('./routes/validation'));
-app.use('/balikaral', require('./routes/practice-examination'));
+app.use('/api/balikaral', require('./routes/users'));
+app.use('/api/balikaral', require('./routes/learningStrand'));
+app.use('/api/balikaral', require('./routes/level'));
+app.use('/api/balikaral', require('./routes/exam-management'));
+app.use('/api/balikaral', require('./routes/exam-type-management'));
+app.use('/api/balikaral', require('./routes/reviewer-management'));
+app.use('/api/balikaral', require('./routes/generated-exam'));
+app.use('/api/balikaral', require('./routes/learningStrandSub'));
+app.use('/api/balikaral', require('./routes/forum'));
+app.use('/api/balikaral', require('./routes/comment'));
+app.use('/api/balikaral', require('./routes/management-forum'));
+app.use('/api/balikaral', require('./routes/examination-result'));
+app.use('/api/balikaral', require('./routes/session-guide-management'));
+app.use('/api/balikaral', require('./routes/survey-management'));
+app.use('/api/balikaral', require('./routes/survey-user'));
+app.use('/api/balikaral', require('./routes/landing-page'));
+app.use('/api/balikaral', require('./routes/auditTrail'));
+app.use('/api/balikaral', require('./routes/site-instruction'));
+app.use('/api/balikaral', require('./routes/teacher-learner'));
+app.use('/api/balikaral', require('./routes/validation'));
+app.use('/api/balikaral', require('./routes/practice-examination'));
 
 
 module.exports = app;
